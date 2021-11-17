9d04527577c7b644840e4e810617ca079da7afe9
ilya <ilya_wot2012@gmail.com>
2021-11-17 22:13:35 +0300

    modal

--- src/js/main.js
+++ src/js/main.js
@@ -84,4 +84,51 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     setClock('.timer', deadline);
+    
+    //Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalCloseBtn = document.querySelector('[data-close]');
+    
+        function openModal() {
+            modal.classList.add('show');
+            document.body.style.overflow = 'hidden';
+            clearInterval(modalTimerId);
+        }
+        function closeModal() {
+            modal.classList.remove('show');
+            document.body.style.overflow = '';
+        }
+
+        modalTrigger.forEach(item => {
+            item.addEventListener('click', () => {
+                openModal();
+            });
+        }); 
+
+        modalCloseBtn.addEventListener('click', closeModal);
+
+        modal.addEventListener('click', (e) => {
+            if(e.target === modal) {
+                closeModal();
+            }
+        });
+
+        document.addEventListener('keydown', (e) => {
+            if(e.code === 'Escape' && modal.classList.contains('show')) {
+                closeModal();
+            }
+        });
+
+        const modalTimerId = setTimeout(openModal, 3000);
+
+        function showModalByScroll () {
+            if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+                openModal();
+                window.removeEventListener('scroll', showModalByScroll);
+            }
+        }
+
+        window.addEventListener('scroll', showModalByScroll);
 });
